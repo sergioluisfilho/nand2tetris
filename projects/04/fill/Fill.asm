1cc4030f3775006593ea158b810797d0ac004353
Sérgio Filho <sergioluisfilho@hotmail.com>
2022-09-02 21:54:50 -0300

    c

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,35 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(INICIO)
+        @SCREEN
+        D=A
+        @i
+        M=D
+(LOOP)
+        @i
+        D=M
+        @24575
+        D=A-D
+        @INICIO
+        D;JLT
+        @KBD
+        D=M
+        @PRETO
+        D;JNE
+        @i
+        D=M
+        A=D
+        M=0
+        @ANDATELA
+        0;JMP
+(PRETO)
+        @i
+        D=M
+        A=D
+        M=-1
+(ANDATELA)
+        @i
+        M=M+1
+        @LOOP
+        0;JMP
